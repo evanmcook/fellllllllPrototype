9cdc77c964dc8bf55b8ec21caf26a671a90908f3
evanmcook <42785294+evanmcook@users.noreply.github.com>
2020-12-13 17:47:32 -0500

    added audio
    now when the paws hit the table, audio playback is delivered from softcut.

--- fellllllll/fel001.lua
+++ fellllllll/fel001.lua
@@ -20,6 +20,9 @@ function init()
 
   two = false
   three = false
+  
+  
+  --softcut.buffer_read_mono("/home/we/dust/audio/common/808/808-BD.wav", 0, 0, -1, 1, 2)
 
   -- Render
   redraw()
@@ -31,16 +34,18 @@ end
 function key(id,state)
   print('key',id,state)
   
-  --if key3 is pressed, set the boolean flag for 3 to true
+  --if key3 is pressed, set the boolean flag for 3 to true (and call a drum to play)
   if (id == 3) and (state==1) then
    three = true
    redraw()
+   rightDrum()
    
   --elseif key2 is pressed, set the boolean flag for 2 to true
   elseif (id==2) and (state==1) then
   --mode = 2
    two = true
    redraw()
+   leftDrum()
   
   --elseif key2 and key 3 are pressed
   --set the boolean flag for 2 to true and...
@@ -86,6 +91,53 @@ function redraw()
   
   end
   screen.update()
+end
+
+-- Audio
+function rightDrum()
+  
+--read the file-to-be-played into softcut's buffer
+softcut.buffer_read_mono("/home/we/dust/audio/common/808/808-SD.wav", 0, 0, -1, 1, 1)
+--set voice enable of voice 1 to 'on'
+softcut.enable(1,1)
+--set voice 1's buffer to buffer 1
+softcut.buffer(1,1)
+--set the output level of voice 1 to 1.0
+softcut.level(1,1.0)
+--set loop mode of voice 1 to on. unclear why the setting the second 1 to 0 means silence.
+softcut.loop(1,1)
+--set the loop start point of voice 1 to 1 sec
+softcut.loop_start(1,1)
+--set the loop end point of voice 1 to 2 sec
+softcut.loop_end(1,2)
+--set the location of the playhead for voice 1 to 0sec
+softcut.position(1,0)
+--turn the play status of voice 1 on
+softcut.play(1,1)
+
+end
+
+function leftDrum()
+    
+--read the file-to-be-played into softcut's buffer. 
+-- filename,start time of file, start of buffer to write in to 
+softcut.buffer_read_mono("/home/we/dust/audio/common/808/808-CH.wav", 0, 3, -1, 1, 1)
+--set voice enable of voice 1 to 'on'
+softcut.enable(1,1)
+--set voice 1's buffer to buffer 1
+softcut.buffer(1,1)
+--set the output level of voice 1 to 1.0
+softcut.level(1,1.0)
+--set loop mode of voice 1 to on. unclear why the setting the second 1 to 0 means silence.
+softcut.loop(1,1)
+--set the loop start point of voice 1 to 1 sec
+softcut.loop_start(1,1)
+--set the loop end point of voice 1 to 2 sec
+softcut.loop_end(1,2)
+--set the location of the playhead for voice 1 to 0sec
+softcut.position(1,3)
+--turn the play status of voice 1 on
+softcut.play(1,1)
 end
 
 -- Utils
